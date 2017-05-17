40ff68c7dc5a059c586398261e55d7eea380e565
Nebbles <bsdgreenberg@gmail.com>
2017-05-17 17:06:35 +0100

    modifications on pi

--- test1.py
+++ test1.py
@@ -30,30 +30,45 @@ GPIO.setup(TRIG2, GPIO.OUT)  # set as I/O output
 GPIO.setup(ECHO1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(ECHO2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+fall = False
+rise = False
+
+def fall_callback(channel):
+    global fall
+    print('Falling edge detected')
+    fall = True
+def rise_callback(channel):
+    global rise
+    print('Rising edge detected')
+    rise = True
+
 try:
     raw_input('Set-up complete. Press any button to continue.')
-
+    GPIO.add_event_detect(ECHO1, GPIO.RISING, callback=rise_callback)
+    GPIO.add_event_detect(ECHO2, GPIO.FALLING, callback=fall_callback)
     while True:
         print('Sending trigger')
         GPIO.output(TRIG1, GPIO.HIGH)  # Trigger goes high
         GPIO.output(TRIG2, GPIO.HIGH)  # Trigger goes high
-        time.sleep(20 / 1000000.0)  # delay by 20 micro-sec
+        #time.sleep(20 / 1000000.0)  # delay by 20 micro-sec
         GPIO.output(TRIG1, GPIO.LOW)  # trigger goes low
         GPIO.output(TRIG2, GPIO.LOW)  # trigger goes low
 
-        while not GPIO.input(ECHO2):  # while echo value low
-            start_time = datetime.datetime.now()  # set the start time to current
+        #GPIO.wait_for_edge(ECHO2, GPIO.RISING)  # while echo value low
+        #start_time = datetime.datetime.now()  # set the start time to current
 
-        while GPIO.input(ECHO2):  # while echo value high
-            end_time = datetime.datetime.now()  # set the end time to current
-
+        #GPIO.wait_for_edge(ECHO2, GPIO.FALLING)  # while echo value high
+        #end_time = datetime.datetime.now()  # set the end time to current
+	while not fall and not rise:
+            pass
         print('ECHO received')
 
-        dt = end_time - start_time  # calculate time difference
-        print(dt)  # print time difference
+        #dt = end_time - start_time  # calculate time difference
+        #print(dt)  # print time difference
 
         # delay and then wait for user prompt to repeat
         raw_input('Press any key to repeat')
-
+except KeyboardInterrupt:
+    GPIO.cleanup()
 finally:
     GPIO.cleanup()
