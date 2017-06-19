e046eb61eff7bd1bad035659e7a4c73f948f0fe6
nebbles <bsg115@ic.ac.uk>
2017-06-19 12:56:13 +0100

    updates

--- MATLAB/EDP_WORKLIKE.m
+++ MATLAB/EDP_WORKLIKE.m
@@ -15,7 +15,7 @@ highpass = 500;
 magnitude_thresh = 5.0;
 maxFs = [];
 hFig = figure('Name','Audio Analysis','NumberTitle','off','Color','white','Units','normalized','Position',[.15 .15 .7 .7]);
-c = uicontrol
+% stop_btn = uicontrol('Style','pushbutton','String','Stop','Callback',@stop_fcn);
 tic;
 stamps = [];
 vols = [];
@@ -25,42 +25,58 @@ breaths = [];
 ts = 0;
 k = 0.09; % linear coefficient for Freq->Vol
 counter = 0;
+isRunning = 1;
+p = 0;
 
 % Create audio recording object.
 obj = audiorecorder(fs,8,1);
 set(obj,'TimerPeriod',tperiod,'TimerFcn',@audioTimer); 
 record(obj);
+% pause(3)
+% pause(obj)
 
+
+pause(1)
 while true % Prevent function from ending unless CTRL+C
     if ~ishghandle(hFig)
         break
     end
-    pause(0.1);
+    if p == 1
+        pause(obj)
+    end
+    pause(0.01)
+end
+    
+function stop_fcn
+    display('working')
+    pause(obj)
 end
 
 function audioTimer(hObj,~)
-    data = getaudiodata(hObj);
-    
+    if isRunning == 1
+        data = getaudiodata(hObj);
+    end
+
     num = fs * tperiod;
     sig = data( length(data) - num +1 : length(data) );
-    
+
     subplot(2,2,1)
 %     plot(sig);
 %     ylim([-0.2 0.2]);
-    
+
     magnitude = abs(fft(sig));
     N = length(sig);
     df = fs / N;
     f = 0:df:fs/2;
     Y = magnitude(1:length(f));
-    
+
 %     subplot(2,2,2)
 %     plot(f, Y/(N)*2);
 %     ylim([0 0.03]);
 %     xlabel('frequency (Hz)');
 %     ylabel('Magnitude');
 %     title('Frequency spectrum (live)');
-    
+
     % for value in sig below HIGHPASS, set to zero (suppress)
     crit_idx = round(highpass / df);
     for i = 1:crit_idx
@@ -73,8 +89,8 @@ function audioTimer(hObj,~)
     ylabel('Magnitude');
     ylim([0 5]);
 
-    
-    
+
+
     % if Y is less than the magnitude threshold value
     [maxY, idx] = max(Y); % find highest peak frequency
     if maxY < magnitude_thresh
@@ -83,7 +99,7 @@ function audioTimer(hObj,~)
         maxF = f(idx); % return corresponding frequency
     end
     maxFs = [maxFs maxF];
-    
+
     subplot(2,2,2)
     plot(maxFs);
     ylim([0 3000]);
@@ -91,16 +107,16 @@ function audioTimer(hObj,~)
     ylabel('Max frequency');
     xlabel('Live time segments');
     title('Freq-Time Graph');
-    
+
     if length(maxFs) > 100
         maxFs = [];
     end
-    
+
     %% Convert frequency to flow rate
-    
+
     flow = maxF * k; % calc flow rate
     flows = [flows flow];
-    
+
     subplot(2,2,3)
     plot(flows);
 %     ylim([0 3000]);
@@ -108,19 +124,19 @@ function audioTimer(hObj,~)
     ylabel('Flow rate (L/min)');
     xlabel('Counter');
     title('Flow-Time Graph');
-    
+
     if length(flows) > 100
 %         flows = [];
         export = transpose(flows);
         csvwrite('flow-time.csv',export)
     end
 
-    
+
     %% Convert flow rate to volume
-    
+
     dt = toc;
     tic;
-    
+
     ts = ts + dt;
 %     if ts > 50
 %         ts = 0;
@@ -131,7 +147,7 @@ function audioTimer(hObj,~)
 
     stamps = [stamps ts];
     vol = vol + flow * dt;
-    
+
     if flow == 0
         counter = counter + 1;
     end
@@ -142,12 +158,12 @@ function audioTimer(hObj,~)
         breaths = [breaths vol];
         vol = 0;
     end
-        
+
     if vol < 0
         vol=0;
     end
     vols = [vols vol];
-    
+
     subplot(2,2,4);
     plot(stamps,vols);
     xlim([0 50]);
@@ -155,7 +171,6 @@ function audioTimer(hObj,~)
     xlabel('Time (sec)');
     ylabel('Volume (mL)');
     title('Volume vs Time');
-    
 end
 
 function cleanMeUp()
